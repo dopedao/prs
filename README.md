351ffd8e4a5ac44a1bd4a1a64d83c426740ff774
faces-of-eth <facesofeth@proton.me>
2022-08-05 18:03:11 -0700

    Continuous testing

--- README.md
+++ README.md
@@ -3,8 +3,19 @@
 ## Commands
 
 ```shell
+# One-off run of tests 
 yarn test
+
+# One-off run of tests with gas estimations
 yarn test:gas
+
+# Continually watch files and re-test on file save
+# (Have to save a file first 😅 …does not run automatically)
+yarn test:watch
+
+# Run hardhat node
 yarn dev
+
+# Do it live!
 yarn deploy
 ```
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-watcher';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
   gasReporter: {
     enabled: true
-  }
+  },
+  watcher: {
+    test: {
+      tasks: [
+        'clean', 
+        { command: 'compile', params: { quiet: true } },
+        { command: 'test', params: { noCompile: true, parallel: true } },
+      ],
+      files: ['./contracts', './test'],
+      ignoredFiles: ['**/.git'],
+      verbose: true,
+    },
+  },
 };
 
 export default config;
