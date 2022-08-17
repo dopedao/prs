635a9de4be41838173384a4f69299c0a6ce9ceb8
Oscar Guertler <oscarguertler@gmail.com>
2022-08-17 22:15:20 +0200

    edit helper methods

--- test/lib/helpers.ts
+++ test/lib/helpers.ts
@@ -10,6 +10,13 @@ export async function deployPrs() {
   return { prsMock, p1, p2 };
 }
 
+export async function deployPaperMock() {
+    await deployments.fixture(['PaperMock']);
+    const paperMock = await ethers.getContract('PaperMock');
+
+    return { paperMock };
+}
+
 export function clearAndHashChoice(choices: CHOICES) {
   const clearChoice = choices + '-' + 'some-un-hackable-password';
   const hashedChoice = ethers.utils.soliditySha256(['string'], [clearChoice]);
@@ -20,17 +27,30 @@ export function clearAndHashChoice(choices: CHOICES) {
   ]
 }
 
-export async function setupGame(etherAmount: string = '0.1') {
+export async function setupGame(paperAmount: string = '0.1') {
   const { prsMock, p1, p2 } = await deployPrs();
+  const { paperMock } = await deployPaperMock();
 
   const gameIndex = 0;
-  const entryFee = ethers.utils.parseEther(etherAmount);
+  const entryFee = ethers.utils.parseEther(paperAmount);
 
   const [ clearChoice, hashedChoice ] = clearAndHashChoice(CHOICES.PAPER);
 
+  // Change paper contract to our mock
+  await prsMock.connect(p1).changePaperContract(paperMock.address);
+
+  // Mint paperAmount
+  await paperMock.connect(p1).mint(entryFee);
+  await paperMock.connect(p2).mint(entryFee);
+
+  // Approve prs to move paper for us
+  await paperMock.connect(p1).approve(prsMock.address, entryFee);
+  await paperMock.connect(p2).approve(prsMock.address, entryFee);
+
   // Load our balance up for p1 + p2
-  await p1.sendTransaction({ to: prsMock.address, value: entryFee });
-  await p2.sendTransaction({ to: prsMock.address, value: entryFee });
+  await prsMock.connect(p1).depositPaper(entryFee);
+  await prsMock.connect(p2).depositPaper(entryFee);
+
   // Start the game now we have a balance
   await prsMock.connect(p1).startGame(hashedChoice, entryFee);
 
@@ -42,5 +62,6 @@ export async function setupGame(etherAmount: string = '0.1') {
     p1,
     p2,
     prsMock,
+    paperMock
   };
 }
