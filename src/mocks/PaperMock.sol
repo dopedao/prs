069ba80841eee1982a715c0d31da50eb52579840
Oscar Guertler <oscarguertler@gmail.com>
2022-08-17 22:09:27 +0200

    add paper mock contract

--- src/mocks/PaperMock.sol
+++ src/mocks/PaperMock.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.12;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PaperMock is ERC20 {
+    constructor() ERC20("PAPIER", "PAPIER") {}
+
+    function mintTo(address target, uint256 amount) public {
+        _mint(target, amount);
+    }
+
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+
+    receive() external payable {} 
+}
